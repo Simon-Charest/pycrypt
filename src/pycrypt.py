23dc02a450b81733800d7b0e1bb0db65a85d0587
Simon Charest <simoncharest@gmail.com>
2020-10-07 17:31:50 -0400

    Adding simplified functions

--- src/pycrypt.py
+++ src/pycrypt.py
@@ -204,6 +204,22 @@ class AESCipher:
         return string
 
 
+def encrypt(string, key='', salt=''):
+    """ Simple encryption function """
+    ciphers = get_ciphers([key])
+    string, mac_tags = encrypt_recursively(ciphers, string, salt, AES.MODE_ECB)
+
+    return string
+
+
+def decrypt(string, key='', salt=''):
+    """ Simple decryption function """
+    ciphers = get_ciphers([key])
+    string = decrypt_recursively(ciphers, string, salt, AES.MODE_ECB)
+
+    return string
+
+
 def intersect(list1, list2):
     return [element for element in list1 if element in list2]
 
